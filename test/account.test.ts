21d8716641753e4402f4dba3fbc190faef89613c
Bas Dijkstra <bas@ontestautomation.com>
2025-05-30 11:39:20 +0200

    Add vitest, add first tests

---
+++ test/account.test.ts
@@ -0,0 +1,26 @@
+import { expect, describe, test } from 'vitest';
+import { Account } from '../src/account';
+
+describe('Account tests', () => {
+
+    test('Deposit', () => {
+
+        let account = new Account('checking');
+    
+        account.deposit(10);
+    
+        expect(account.balance).toEqual(10);
+    })
+    
+    test('Deposit, then withdraw', () => {
+    
+        let account = new Account('savings');
+    
+        account.deposit(10);
+    
+        account.withdraw(5);
+    
+        expect(account.balance).toEqual(5);
+    })
+})
+
