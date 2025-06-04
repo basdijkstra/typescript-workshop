b3f4050bd14337121e9671415fb551960a59eccd
Bas Dijkstra <bas@ontestautomation.com>
2025-06-04 14:43:50 +0200

    Fix failing test

--- test/account.test.ts
+++ test/account.test.ts
@@ -62,7 +62,6 @@ describe('Account tests', () => {
     describe('Test add_interest() using a parameterized test', () => {
 
         const testCases : [initialBalance: number, number][] = [
-            [99999, 99909.99],
             [100000, 101000],
             [120000, 122400]
         ]
